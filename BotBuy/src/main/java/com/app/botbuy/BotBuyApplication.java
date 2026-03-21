499eeadfc727f468d28594ba7206147a21e643d3
chenlong <clbigdata@163.com>
2026-03-21 23:40:31 +0800

    更新

---
+++ BotBuy/src/main/java/com/app/botbuy/BotBuyApplication.java
@@ -0,0 +1,13 @@
+package com.app.botbuy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BotBuyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BotBuyApplication.class, args);
+    }
+
+}
