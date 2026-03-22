add237f4a1800fa4a263c47d5f22b1d6febbd6dd
chenlong <clbigdata@163.com>
2026-03-22 16:04:05 +0800

    初始化项目

--- src/main/java/com/app/botbuy/BotBuyApplication.java
+++ src/main/java/com/app/botbuy/BotBuyApplication.java
@@ -1,9 +1,12 @@
 package com.app.botbuy;
 
+import com.app.botbuy.config.BotBuyPromptProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(BotBuyPromptProperties.class)
 public class BotBuyApplication {
 
     public static void main(String[] args) {
